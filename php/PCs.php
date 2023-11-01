569354bfbd80bdb1ae48cb065def83bd516906bc
Grendel99z <112167202+Grendel99z@users.noreply.github.com>
2023-11-02 00:09:48 +0800

    changing all to .php but nevertouch customization

---
+++ php/PCs.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Alpha Z template</title>
+		<link rel="stylesheet" type="text/css" href="../style.css" />
+		<link rel="stylesheet" type="text/css" href="../css/PCs.css" />
+	</head>
+
+	<body>
+		<div class="wrapper">
+			<div id="header">
+				<?php
+					include_once 'header.php';
+				?>
+			</div>
+			<div id="defaultSidePane">
+					<?php
+						include 'sidePane.php';
+					?>
+			</div>
+			<div id="body">
+			<div id="topofpc">
+					<h1>PCs</h1>
+					<br />
+					Our PCs are designed with future-proofing in mind, allowing for
+					easy<br />
+					upgrades and expansion, so you can keep up with the ever-evolving<br />
+					technology landscape.
+					<br /><br /><br /><br />
+				</div>
+
+				<div>
+					<h2>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;VALUE EDITION
+					</h2>
+				</div>
+
+				<div id="pcvalue">
+					<div class="product-list-container">
+						<div class="product-card">
+							<img
+								src="../assets/zenith1.png"
+								width="400px"
+								height="400px"
+								alt="Product Name"
+							/>
+							<h3 class="product-title">Zenith 1</h3>
+							<p class="product-description">Zenith 1 is suitable for tasks like office productivity,<br> web browsing, and even gaming.</p>
+							<p class="product-price">$1500.00</p>
+							<p class="product-button"><br>CUSTOMIZE</p>
+						</div>&nbsp;
+						<div class="product-card">
+							<img
+								src="../assets/zenith2.png"
+								width="400px"
+								height="400px"
+								alt="Product Name"
+							/>
+							<h3 class="product-title">Zenith 2</h3>
+							<p class="product-description">Zenith 2 is suitable for tasks like multi-tasking, watching<br>movies, and even gaming.</p>
+							<p class="product-price">$1600.00</p>
+							<p class="product-button"><br>CUSTOMIZE</p>
+						</div>
+					</div>
+
+					<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+					<div class="product-list-container">
+						<div class="product-card">
+							<img
+								src="../assets/zenith3.png"
+								width="435px"
+								height="400px"
+								alt="Product Name"
+							/>
+							<h3 class="product-title">Zenith 3</h3>
+							<p class="product-description">Zenith 3 is a mid range PC that can achieve quality gaming<br> experience.</p>
+							<p class="product-price">$1700.00</p>
+							<p class="product-button"><br>CUSTOMIZE</p>
+						</div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						<div class="product-card">
+							<img
+								src="../assets/zenith4.png"
+								width="400px"
+								height="400px"
+								alt="Product Name"
+							/>
+							<h3 class="product-title">Zenith 4</h3>
+							<p class="product-description">Zenith 4 is a mid range PC for handling vigorous office applications,<br>and even gaming.</p>
+							<p class="product-price">$1800.00</p>
+							<p class="product-button"><br>CUSTOMIZE</p>
+						</div>
+					</div>
+
+					<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+				</div>
+
+				<div>
+					<h2>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PREMIUM EDITION
+					</h2>
+				</div>
+
+				<div id="pcpremium">
+					<div class="product-list-container">
+						<div class="product-card">
+							<img
+								src="../assets/zenith5.png"
+								width="400px"
+								height="400px"
+								alt="Product Name"
+							/>
+							<h3 class="product-title">Zenith 5</h3>
+							<p class="product-description">Zenith 5 is a high-end PC that all gamers wish for,<br>it can handle all operations smoothly, especially gaming.</p>
+							<p class="product-price">$3000.00</p>
+							<p class="product-button"><br>CUSTOMIZE</p>
+						</div>&nbsp;
+						<div class="product-card">
+							<img
+								src="../assets/zenith6.png"
+								width="400px"
+								height="400px"
+								alt="Product Name"
+							/>
+							<h3 class="product-title">Zenith 6</h3>
+							<p class="product-description">Zenith 6 is a high-end PC that all gamers wish for,<br>it can handle all operations exceptionally well, especially gaming.</p>
+							<p class="product-price">$4000.00</p>
+							<p class="product-button"><br>CUSTOMIZE</p>
+						</div>
+					</div>
+
+					<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+				</div>
+			</div>
+			<div id="footer"></div>
+		</div>
+	</body>
+	<script src="../js/sidePane.js"></script>
+	<script src="../js/footer.js"></script>
+</html>
