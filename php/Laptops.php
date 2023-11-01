0b0dcf7008e7478962faba33976b38f54a1a4151
SOH JUN YANG RYNARD <112167202+Grendel99z@users.noreply.github.com>
2023-11-02 00:10:21 +0800

    changing all to .php but nevertouch customization (#37)

---
+++ php/Laptops.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Alpha Z template</title>
+		<link rel="stylesheet" type="text/css" href="../style.css" />
+		<link rel="stylesheet" type="text/css" href="../css/Laptops.css" />
+	</head>
+
+	<body>
+		<div class="wrapper">
+			<div id="header">
+				<?php
+					include_once 'header.php';
+				?>
+			</div>
+			<div id="defaultSidePane">
+					<?php
+						include 'sidePane.php';
+					?>
+			</div>
+			<div id="body">
+			<div id="topoflaptop">
+					<h1>Laptops</h1>
+					<br />
+					Discover the power and versatility of our Alpha Z Laptops, your
+					gateway<br />to a seemless computing experience Whether you're a
+					professional, a student,<br />
+					or a tech enthusiast, this laptop is designed to meet your needs and
+					exceed your expectations.
+					<br /><br /><br /><br />
+				</div>
+
+				<div>
+					<h2>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;VALUE EDITION (Pre-Built)
+					</h2>
+				</div>
+
+				<div id="laptopvalue">
+					<div class="product-list-container">
+						<div class="product-card">
+							<img
+								src="../assets/titan1.jpg"
+								width="450px"
+								height="375px"
+								alt="Product Name"
+							/>
+							<h3 class="product-title">Titan 1</h3>
+							<p class="product-description">Titan 1 is suitable for tasks like office productivity, web<br>browsing, and even gaming.</p>
+							<p class="product-price">$1000.00</p>
+							<p class="product-button"><br>VIEW PRODUCT</p>
+						</div>&nbsp;
+						<div class="product-card">
+							<img
+								src="../assets/titan2.jpg"
+								width="450px"
+								height="375px"
+								alt="Product Name"
+							/>
+							<h3 class="product-title">Titan 2</h3>
+							<p class="product-description">Titan 2 is suitable for tasks like multi-tasking, watching<br>movies, and even gaming.</p>
+							<p class="product-price">$1100.00</p>
+							<p class="product-button"><br>VIEW PRODUCT</p>
+						</div>
+					</div>
+
+					<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+					<div class="product-list-container">
+						<div class="product-card">
+							<img
+								src="../assets/titan3.jpg"
+								width="450px"
+								height="315px"
+								alt="Product Name"
+							/>
+							<h3 class="product-title">Titan 3</h3>
+							<p class="product-description">Titan 3 is a mid range laptop that can achieve quality gaming experience.</p>
+							<p class="product-price">$1200.00</p>
+							<p class="product-button"><br>VIEW PRODUCT</p>
+						</div>&nbsp;
+						<div class="product-card">
+							<img
+								src="../assets/titan4.jpg"
+								width="450px"
+								height="315px"
+								alt="Product Name"
+							/>
+							<h3 class="product-title">Titan 4</h3>
+							<p class="product-description">Titan 4 is a mid range laptop for handling vigorous office applications,<br>and even gaming.</p>
+							<p class="product-price">$1300.00</p>
+							<p class="product-button"><br>VIEW PRODUCT</p>
+						</div>
+					</div>
+				</div>
+
+				<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+				<div>
+					<h2>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PREMIUM EDITION
+						(Pre-Built)
+					</h2>
+				</div>
+
+				<div id="laptoppremium">
+					<div class="product-list-container">
+						<div class="product-card">
+							<img
+								src="../assets/titan5.png"
+								width="450px"
+								height="355px"
+								alt="Product Name"
+							/>
+							<h3 class="product-title">Titan 5</h3>
+							<p class="product-description">Titan 5 is a high-end laptop that all gamers wish for,<br>it can handle all operations smoothly, especially gaming.</p>
+							<p class="product-price">$2000.00</p>
+							<p class="product-button"><br>VIEW PRODUCT</p>
+						</div>&nbsp;
+						<div class="product-card">
+							<img
+								src="../assets/titan6.png"
+								width="450px"
+								height="355px"
+								alt="Product Name"
+							/>
+							<h3 class="product-title">Titan 6</h3>
+							<p class="product-description">Titan 6 is a high-end laptop that all gamers wish for,<br>it can handle all operations exceptionally well, especially gaming.</p>
+							<p class="product-price">$2500.00</p>
+							<p class="product-button"><br>VIEW PRODUCT</p>
+						</div>
+					</div>
+				</div>
+				
+				<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+			</div>
+			<div id="footer"></div>
+		</div>
+	</body>
+	<script src="../js/sidePane.js"></script>
+	<script src="../js/footer.js"></script>
+</html>
