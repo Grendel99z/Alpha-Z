828148ce9ac84d7cdcc1d86eb78ec31c8e656dbc
Grendel99z <112167202+Grendel99z@users.noreply.github.com>
2023-11-04 22:19:58 +0800

    fix usersql

--- db/user.sql
+++ db/user.sql
@@ -2,9 +2,10 @@ CREATE TABLE `user` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
+    `phone_number` VARCHAR(255) NOT NULL,
     `address` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`));
 
-INSERT INTO user (name, email, address, password)
-VALUES ('rynard', 'alpha@hotmail.com', 'tampines', 'testing123');
+INSERT INTO user (name, email, phone_number, address, password)
+VALUES ('rynard', 'alpha@hotmail.com', '12345678', 'tampines', 'testing123');
